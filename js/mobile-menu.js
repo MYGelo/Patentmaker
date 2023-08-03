a25530794c95a3d42417d6a52586afc1dd5ca946
MYGelo(MiholapOleg) <oleg.miholap95@gmail.com>
2023-08-03 17:41:02 +0300

    add search

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -3,15 +3,33 @@
   const openMenuBtn = document.querySelector('.js-open-menu');
   const closeMenuBtn = document.querySelector('.js-close-menu');
 
+  const searchGlass = document.querySelector('#searchGlass');
+  const searchMenu = document.querySelector('.searchMenu');
+  const searchBtn = document.querySelector('#searchBtn');
+
+  const searchGlassMenu = () => {
+    console.log('click search');
+
+    const isMenuOpen = searchGlass.getAttribute('aria-expanded') === 'true' || false;
+    searchGlass.setAttribute('aria-expanded', !isMenuOpen);
+
+    searchMenu.classList.toggle('is-open');
+  };
+
+  const searchBtnPress = () => {
+    console.log(`srearch BTN`);
+    searchGlassMenu();
+  };
+
+  searchGlass.addEventListener('click', searchGlassMenu);
+  searchBtn.addEventListener('click', searchBtnPress);
+
   const toggleMenu = () => {
-    const isMenuOpen =
-      openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
+    const isMenuOpen = openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
     openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
     mobileMenu.classList.toggle('is-open');
 
-    const scrollLockMethod = !isMenuOpen
-      ? 'disableBodyScroll'
-      : 'enableBodyScroll';
+    const scrollLockMethod = !isMenuOpen ? 'disableBodyScroll' : 'enableBodyScroll';
     bodyScrollLock[scrollLockMethod](document.body);
   };
 
@@ -19,7 +37,7 @@
   closeMenuBtn.addEventListener('click', toggleMenu);
 
   // Close the mobile menu on wider screens if the device orientation changes
-  window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
+  window.matchMedia('(min-width: 1240px)').addEventListener('change', e => {
     if (!e.matches) return;
     mobileMenu.classList.remove('is-open');
     openMenuBtn.setAttribute('aria-expanded', false);
